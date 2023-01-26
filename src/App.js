bbc5aa4404d1f14e8940c4edeab173c909f0d21c
Cipher-20 <kan20_2003@abv.bg>
2023-01-26 17:39:18 +0200

    create style of form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+import FormComponent from './components/FormComponent/FormComponent';
+
 function App() {
   return (
     <div className="App">
 
+      <FormComponent/>
     </div>
   );
 }
